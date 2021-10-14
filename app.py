d6c51d7f5f517e7a3a5dae0d061a62a555ea75ec
Aldwin <aldwin828@gmail.com>
2021-10-13 21:15:13 -0400

    Added finally

--- app.py
+++ app.py
@@ -75,22 +75,34 @@ def posts():
     except:
         print("Something went wrong")
     finally:
-        if (len(sys.argv) > 1):
-            mode = sys.argv[1]
-            if (mode == "production"):
-                import bjoern
-                host = '0.0.0.0'
-                port = 5000
-                print("Server is running in production mode")
-                bjoern.run(app, host, port)
-            elif(mode == "testing"):
-                from flask_cors import CORS
-                CORS(app)
-                print("Server is running in testing mode, switch to production when needed")
-                app.run(debug=True) 
-            else:
-                print("Invalid mode argument, exiting")
-                exit() 
+        if (cursor != None):
+            cursor.close()
         else:
-            print("No argument was provided")
-            exit()
+            print("There was never a cursor to begin with")
+        # Check the connection
+        if (conn != None):
+            conn.rollback()
+            conn.close()
+        else:
+            print("The connection never opened, nothing to close here")
+
+
+if (len(sys.argv) > 1):
+    mode = sys.argv[1]
+    if (mode == "production"):
+        import bjoern
+        host = '0.0.0.0'
+        port = 5000
+        print("Server is running in production mode")
+        bjoern.run(app, host, port)
+    elif(mode == "testing"):
+        from flask_cors import CORS
+        CORS(app)
+        print("Server is running in testing mode, switch to production when needed")
+        app.run(debug=True) 
+    else:
+        print("Invalid mode argument, exiting")
+        exit() 
+else:
+    print("No argument was provided")
+    exit()
