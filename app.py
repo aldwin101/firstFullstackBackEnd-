f8094fcb8e50465f610a2e1e0cb8323f65534caf
Aldwin <aldwin828@gmail.com>
2021-10-13 20:26:00 -0400

    Fixed cursor error

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ app = Flask(__name__)
     # posts handler
 def posts():
     try:
+        cursor = None
+        conn = None
+        
         conn = mariadb.connect(user=dbcreds.user,
                             password=dbcreds.password,
                             host=dbcreds.host,
